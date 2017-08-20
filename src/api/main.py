05461b41ad093743ea63e8a5c2b1c34b96c4f234
squirrelRao <hqraop@163.com>
2017-08-20 15:56:52 +0800

    update env configuration and code

--- src/api/main.py
+++ src/api/main.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
-
+from flask import request
+from logging.handlers import TimedRotatingFileHandler
+from util import api
+from caiyun.platform.redis_client import client as rc
+from caiyun.platform.mongo_client import client as mongo_client
 application = Flask(__name__)
 route = application.route
 application.debug = True
 
-@route('/')
+@route('/v1/')
 def index():
-	return "Hello World"
+	return "Hello LEAD`"
 
-@route('/curtime')
+@route('/v1/curtime')
+@api
 def curtime():
-	return '{"time": "2015-06-22 12:00"}'
+	return {"time": "2015-06-22 12:00"}
 
 def main():
 	application.debug = True
