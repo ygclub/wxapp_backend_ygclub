6d27fb5ff8125d0c607468c26e9347931a10d32b
squirrelRao <hqraop@163.com>
2017-08-20 15:57:15 +0800

    add util.py

---
+++ src/api/util.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import json
+import flask
+
+from functools import wraps
+
+def api(func):
+    """Wraps JSONified output for JSONP requests."""
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        try:
+            value = func(*args, **kwargs)
+        except BaseException as e:
+            sentry.captureException()
+            value = {
+                "status": "failed",
+                "error": "Unkonwn error!"
+            }
+            flask.current_app.logger.error("Unkonwn error: %s" % str(e))
+            flask.current_app.logger.exception(e)
+
+        status = 200
+        mimetype = 'application/json'
+        content = json.dumps(value)
+        callback = flask.request.args.get('callback', False)
+        if callback:
+            content = str(callback) + '(' + content + ')'
+            mimetype = 'application/javascript'
+
+        return flask.current_app.response_class(content, status=status, mimetype=mimetype)
+    return decorated_function
